2c1857d54326ee4ac70b606fb7ef8c237a922c10
Nikolaj Raagart <nikolaj@raagart.dk>
2023-09-19 18:05:24 +0200

    Create monthvalue.java
    exercise 6.5

---
+++ monthvalue.java
@@ -0,0 +1,11 @@
+class monthvalue
+{
+	public static void main(String[] args)
+	{
+		float monthValue = 31.0f;
+		System.out.println(monthValue);
+		monthValue += 0.5f;
+		System.out.println(monthValue);
+
+	}
+}
