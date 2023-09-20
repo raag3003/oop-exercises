2ab468a3f00674eda5979edd9202c0ef74fea182
Nikolaj Raagart <nikolaj@raagart.dk>
2023-09-20 12:03:22 +0200

    new creations

---
+++ dice.java
@@ -0,0 +1,13 @@
+class dice{
+	public static void main(String[] args)
+	{
+		int dice = 4;
+		boolean diceMin = false;
+
+		if (dice >= 3)
+		{
+			diceMin = true;
+		}
+		System.out.println(diceMin);
+	}
+}
